04b353ed486944fe8713e224c94a143a3fa44738
luisbandivas <jimboybandivas@gmail.com>
2024-09-04 22:13:19 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="h-svh bg-[#232323] flex">
+        <h1 className="text-[#F5F5F5] font-playfair text-xl">
+          <span className="text-[#16A085]">L</span>UIS{" "}
+          <span className="text-[#16A085]">M</span>ARIO BANDIVAS
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
